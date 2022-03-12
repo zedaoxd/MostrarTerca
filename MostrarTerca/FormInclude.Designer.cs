fffa52b56bc7959bd244921b2508e6043606d432
Bruno Lessa <brunolessa@live.com>
2022-03-12 19:00:51 -0300

    screen add

--- MostrarTerca/FormInclude.Designer.cs
+++ MostrarTerca/FormInclude.Designer.cs
@@ -29,6 +29,23 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.label10 = new System.Windows.Forms.Label();
+            this.textBoxBrand = new System.Windows.Forms.TextBox();
+            this.textBoxModel = new System.Windows.Forms.TextBox();
+            this.textBoxYear = new System.Windows.Forms.TextBox();
+            this.textBoxYearManufacture = new System.Windows.Forms.TextBox();
+            this.textBoxColor = new System.Windows.Forms.TextBox();
+            this.textBoxPrice = new System.Windows.Forms.TextBox();
+            this.comboBoxAutomatic = new System.Windows.Forms.ComboBox();
+            this.comboBoxFuel = new System.Windows.Forms.ComboBox();
+            this.buttonSave = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label1
@@ -42,12 +59,222 @@
             this.label1.TabIndex = 0;
             this.label1.Text = "Add Vehicle";
             // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(162, 148);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(59, 19);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Brand:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.White;
+            this.label4.Location = new System.Drawing.Point(420, 324);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(45, 19);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Fuel:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.ForeColor = System.Drawing.Color.White;
+            this.label5.Location = new System.Drawing.Point(127, 324);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(94, 19);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Automatic:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.ForeColor = System.Drawing.Color.White;
+            this.label6.Location = new System.Drawing.Point(360, 231);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(172, 19);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Year of manufacture:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.ForeColor = System.Drawing.Color.White;
+            this.label7.Location = new System.Drawing.Point(170, 362);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(51, 19);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Price:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.ForeColor = System.Drawing.Color.White;
+            this.label8.Location = new System.Drawing.Point(167, 277);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(54, 19);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Color:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.ForeColor = System.Drawing.Color.White;
+            this.label9.Location = new System.Drawing.Point(172, 231);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(49, 19);
+            this.label9.TabIndex = 8;
+            this.label9.Text = "Year:";
+            // 
+            // label10
+            // 
+            this.label10.AutoSize = true;
+            this.label10.Font = new System.Drawing.Font("Century Gothic", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label10.ForeColor = System.Drawing.Color.White;
+            this.label10.Location = new System.Drawing.Point(159, 192);
+            this.label10.Name = "label10";
+            this.label10.Size = new System.Drawing.Size(62, 19);
+            this.label10.TabIndex = 9;
+            this.label10.Text = "Model:";
+            // 
+            // textBoxBrand
+            // 
+            this.textBoxBrand.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.textBoxBrand.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxBrand.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxBrand.ForeColor = System.Drawing.Color.White;
+            this.textBoxBrand.Location = new System.Drawing.Point(227, 146);
+            this.textBoxBrand.Name = "textBoxBrand";
+            this.textBoxBrand.Size = new System.Drawing.Size(444, 26);
+            this.textBoxBrand.TabIndex = 10;
+            // 
+            // textBoxModel
+            // 
+            this.textBoxModel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.textBoxModel.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxModel.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxModel.ForeColor = System.Drawing.Color.White;
+            this.textBoxModel.Location = new System.Drawing.Point(227, 190);
+            this.textBoxModel.Name = "textBoxModel";
+            this.textBoxModel.Size = new System.Drawing.Size(444, 26);
+            this.textBoxModel.TabIndex = 11;
+            // 
+            // textBoxYear
+            // 
+            this.textBoxYear.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.textBoxYear.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxYear.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxYear.ForeColor = System.Drawing.Color.White;
+            this.textBoxYear.Location = new System.Drawing.Point(227, 231);
+            this.textBoxYear.Name = "textBoxYear";
+            this.textBoxYear.Size = new System.Drawing.Size(127, 26);
+            this.textBoxYear.TabIndex = 12;
+            // 
+            // textBoxYearManufacture
+            // 
+            this.textBoxYearManufacture.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.textBoxYearManufacture.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxYearManufacture.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxYearManufacture.ForeColor = System.Drawing.Color.White;
+            this.textBoxYearManufacture.Location = new System.Drawing.Point(538, 229);
+            this.textBoxYearManufacture.Name = "textBoxYearManufacture";
+            this.textBoxYearManufacture.Size = new System.Drawing.Size(133, 26);
+            this.textBoxYearManufacture.TabIndex = 13;
+            // 
+            // textBoxColor
+            // 
+            this.textBoxColor.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.textBoxColor.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxColor.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxColor.ForeColor = System.Drawing.Color.White;
+            this.textBoxColor.Location = new System.Drawing.Point(227, 275);
+            this.textBoxColor.Name = "textBoxColor";
+            this.textBoxColor.Size = new System.Drawing.Size(186, 26);
+            this.textBoxColor.TabIndex = 14;
+            // 
+            // textBoxPrice
+            // 
+            this.textBoxPrice.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.textBoxPrice.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBoxPrice.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPrice.ForeColor = System.Drawing.Color.White;
+            this.textBoxPrice.Location = new System.Drawing.Point(227, 360);
+            this.textBoxPrice.Name = "textBoxPrice";
+            this.textBoxPrice.Size = new System.Drawing.Size(186, 26);
+            this.textBoxPrice.TabIndex = 15;
+            // 
+            // comboBoxAutomatic
+            // 
+            this.comboBoxAutomatic.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.comboBoxAutomatic.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxAutomatic.ForeColor = System.Drawing.Color.White;
+            this.comboBoxAutomatic.FormattingEnabled = true;
+            this.comboBoxAutomatic.Items.AddRange(new object[] {
+            "Yes",
+            "No"});
+            this.comboBoxAutomatic.Location = new System.Drawing.Point(228, 324);
+            this.comboBoxAutomatic.Name = "comboBoxAutomatic";
+            this.comboBoxAutomatic.Size = new System.Drawing.Size(121, 28);
+            this.comboBoxAutomatic.TabIndex = 16;
+            // 
+            // comboBoxFuel
+            // 
+            this.comboBoxFuel.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
+            this.comboBoxFuel.Font = new System.Drawing.Font("Century Gothic", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.comboBoxFuel.ForeColor = System.Drawing.Color.White;
+            this.comboBoxFuel.FormattingEnabled = true;
+            this.comboBoxFuel.Location = new System.Drawing.Point(471, 321);
+            this.comboBoxFuel.Name = "comboBoxFuel";
+            this.comboBoxFuel.Size = new System.Drawing.Size(121, 28);
+            this.comboBoxFuel.TabIndex = 17;
+            // 
+            // buttonSave
+            // 
+            this.buttonSave.FlatAppearance.BorderSize = 0;
+            this.buttonSave.FlatAppearance.MouseOverBackColor = System.Drawing.Color.Green;
+            this.buttonSave.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.buttonSave.Font = new System.Drawing.Font("Century Gothic", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSave.ForeColor = System.Drawing.Color.White;
+            this.buttonSave.Location = new System.Drawing.Point(364, 443);
+            this.buttonSave.Name = "buttonSave";
+            this.buttonSave.Size = new System.Drawing.Size(142, 44);
+            this.buttonSave.TabIndex = 18;
+            this.buttonSave.Text = "Save";
+            this.buttonSave.UseVisualStyleBackColor = true;
+            // 
             // FormInclude
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(49)))), ((int)(((byte)(66)))), ((int)(((byte)(82)))));
             this.ClientSize = new System.Drawing.Size(1048, 537);
+            this.Controls.Add(this.buttonSave);
+            this.Controls.Add(this.comboBoxFuel);
+            this.Controls.Add(this.comboBoxAutomatic);
+            this.Controls.Add(this.textBoxPrice);
+            this.Controls.Add(this.textBoxColor);
+            this.Controls.Add(this.textBoxYearManufacture);
+            this.Controls.Add(this.textBoxYear);
+            this.Controls.Add(this.textBoxModel);
+            this.Controls.Add(this.textBoxBrand);
+            this.Controls.Add(this.label10);
+            this.Controls.Add(this.label9);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "FormInclude";
@@ -60,5 +287,22 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.TextBox textBoxBrand;
+        private System.Windows.Forms.TextBox textBoxModel;
+        private System.Windows.Forms.TextBox textBoxYear;
+        private System.Windows.Forms.TextBox textBoxYearManufacture;
+        private System.Windows.Forms.TextBox textBoxColor;
+        private System.Windows.Forms.TextBox textBoxPrice;
+        private System.Windows.Forms.ComboBox comboBoxAutomatic;
+        private System.Windows.Forms.ComboBox comboBoxFuel;
+        private System.Windows.Forms.Button buttonSave;
     }
 }
